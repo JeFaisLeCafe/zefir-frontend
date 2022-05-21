f392c381f3cb1eb063ec8254e8bc9fbba8e739f7
Pierre-Etienne Soury <pierre.etienne.soury@blank.app>
2022-05-21 17:34:48 +0200

    question 2: Fixed homes.tsx

--- pages/homes.tsx
+++ pages/homes.tsx
@@ -1,20 +1,26 @@
 import type { NextPage } from 'next';
 import { useEffect, useState } from 'react';
-
 import { HomeList } from '../components/HomeList';
 import { Layout } from '../components/Layout';
 import { AnimatedLoader } from '../components/AnimatedLoader';
+import { getRandomHomes } from '../business/getRandomHomes';
 
 const Homes: NextPage = () => {
-  const [homes, setHomes] = useState();
-  const [isLoading, setLoading] = useState(false);
+  const [homes, setHomes] = useState<Home[]>([]);
+  const [isLoading, setLoading] = useState(true);
+
+  const fetchHomes = async () => {
+    setLoading(true)
+    console.log('loading')
+    const homes = await getRandomHomes();
+    console.log('homes', homes)
+    setHomes(homes);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    fetch('api/home')
-      .then((res) => res.json())
-      .then((data: Home[]) => setHomes(data));
-    setLoading(false);
-  }, [homes]);
+    fetchHomes()
+  }, []);
 
   if (isLoading) return <AnimatedLoader />
   if (!homes) return <p>No home to show, please reload the page!</p>
